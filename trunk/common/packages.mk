f741378012ad60f5c35ff28895bbfa802ffaf4c6
thomas.gervais@gmail.com <thomas.gervais@gmail.com@11d7e932-b646-b3fd-9e7c-a6633acaa9e0>
2012-08-04 23:52:33 +0000

    save

--- trunk/common/packages.mk
+++ trunk/common/packages.mk
@@ -12,14 +12,18 @@ SRC_NAME=$(basename $(SRC_FILE))
 SPK_FILES:=scripts WIZARD_UIFILES LICENCE PACKAGE_ICON.PNG
 SPK_FLAGS:=--owner=root --group=root --numeric-owner --exclude-vcs
 
+PREFIX=$(CURDIR)/arch-$*
+
+$(info Ready for $(PACKAGE)-$(VERSION))
+
 $(PACKAGE)-$(VERSION)-%.spk: %/package.tgz %/INFO
 	@echo 'Making $@...'
 	@rm -f $@
 	@for file in $^ $(SPK_FILES); do if test -s $$file; then tar ${SPK_FLAGS} -rhf $@ -C $$(dirname $$file) $$(basename $$file) && echo "$$file added"; fi; done
 
-%/package.tgz: %
+%/package.tgz: arch-%
 	@echo 'Making $@...'
-	@tar ${SPK_FLAGS} -zcvf $@ -C $* $$(ls $*)
+	@tar ${SPK_FLAGS} -zcvf $@ -C $< $$(ls $<)
 
 arm-none-linux-gnueabi/INFO: ARCH=88f6281 88f6258
 %/INFO:
@@ -28,7 +32,7 @@ arm-none-linux-gnueabi/INFO: ARCH=88f6281 88f6258
 		 -e 's/$$model/\"$(MODEL)\"/' \
 		 -e 's/^#.*$$//' INFO | tr -s "\n" > $@
 		 
-%: configure-% build-% install-%
+arch-%: configure-% build-% install-%
 	@echo 'Done for $(PACKAGE)-$(VERSION) ($*)'
 
 build-%: 
